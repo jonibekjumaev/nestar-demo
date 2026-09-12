54284cecfd033fd9156d2b41e57f2c0d5d231b6b
Jonny <jonik0744clash@email.com>
2026-09-12 21:59:32 +0900

    feat: Sass integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
+import "../scss/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
@@ -16,3 +17,11 @@ export default function App({ Component, pageProps }: AppProps) {
     </ThemeProvider>
   );
 }
+
+/**
+ *  <ThemeProvider theme={theme}>  // MUI theme obyektini butun ilova bo'ylab barcha komponentlarga tarqatadi (Context orqali), shu tufayli har bir MUI komponenti bitta umumiy dizayn sozlamasini "biladi".
+      <CssBaseline /> // brauzerlarning standart CSS farqlarini (margin, padding, shrift) tozalab, bir xil boshlang'ich holatga keltiradi.
+      <Component {...pageProps} />
+    </ThemeProvider>
+ * 
+ */
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,22 @@
 import { Box, Container, Stack } from "@mui/material";
-import { brown, green } from "@mui/material/colors";
 import { NextPage } from "next";
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
 
 const Home: NextPage = () => {
   return (
-    <Container>
+    <Stack>
       <Stack flexDirection={"column"}>
-        <Box>Popular properties</Box>
-        <Box>Top Agents</Box>
-        <Box>Top properties</Box>
-        <Box>Events</Box>
+        <Stack>
+          <Stack className="container">Popular properties</Stack>
+        </Stack>
+        <Stack>
+          <Stack className="container">Top Agents</Stack>
+        </Stack>
+        <Stack>
+          <Stack className="container">Top properties</Stack>
+        </Stack>
       </Stack>
-    </Container>
+    </Stack>
   );
 };
 
