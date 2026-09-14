42c4cd9293f941a001848125c8cf5834a0a6e1c0
Jonny <jonik0744clash@email.com>
2026-09-14 17:03:24 +0900

    feat: develop apollo client connection and publishing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,11 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
 import "../scss/app.scss";
+import "../scss/pc/main.scss";
+import "../scss/mobile/main.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
@@ -11,10 +15,12 @@ export default function App({ Component, pageProps }: AppProps) {
 
   // Socket.io, Redux, Mui ... Globat integration page
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
--- pages/agent/index.tsx
+++ pages/agent/index.tsx
@@ -1,19 +1,20 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { NextPage } from "next";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 import { Stack } from "@mui/material";
+import { NextPage } from "next";
 
-const Community: NextPage = () => {
+const Agent: NextPage = () => {
   const device = useDeviceDetect();
 
   if (device === "mobile") {
-    return <Stack>COMMUNITY MOBILE</Stack>;
+    return <Stack>AGENT LIST MOBILE</Stack>;
   } else {
     return (
       <div style={{ margin: "20px 0" }}>
-        <Stack className="container">COMMUNITY</Stack>
+        <Stack className="container">AGENTS LIST</Stack>
       </div>
     );
   }
 };
-export default withLayoutBasic(Community);
+
+export default withLayoutBasic(Agent);
